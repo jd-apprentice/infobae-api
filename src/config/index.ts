ee03d1726fde2eeef65b49efd9bfb4b8095ee0cc
jd-apprentice <contacto@jonathan.com.ar>
2024-11-21 00:12:21 -0300

    fix: forgot to resolve conflicts

--- src/config/index.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-    app: {
-        port: process.env.PORT || String(3000)
-    }
-};
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { app } from './server';
-import { config } from '#config';
-
-const startupMessage = 'Infobae API';
-
-app.listen(config.app.port, (): void => console.info(startupMessage));
