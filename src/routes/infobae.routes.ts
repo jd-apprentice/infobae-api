fac7387fb9706b01b92337e6f7dc2b3490f2c203
jd-apprentice <contacto@jonathan.com.ar>
2024-11-21 00:02:46 -0300

    refactor: prep to build app on golang

--- src/routes/infobae.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-const infobaeRoutes = Router();
-import InfobaeController from "../controllers/infobaeController";
-
-infobaeRoutes.get("/", InfobaeController.getPosts);
-
-export default infobaeRoutes;
