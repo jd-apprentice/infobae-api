301207b498ad4344a0c4bef2f8778641165b5ada
jd-apprentice <contacto@jonathan.com.ar>
2024-11-22 00:01:32 -0300

    feat: startup with gin and some methods

--- src/controllers/infobae.controller.go
+++ src/controllers/infobae.controller.go
@@ -1,4 +1,17 @@
 package controllers
 
-// TODO: HANDLE REQUESTS
-func InfobaeController() {}
+import "github.com/gin-gonic/gin"
+
+// TODO: Transform to JSON and return a random post
+func RandomInfobaePost(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Random Infobae Post",
+	})
+}
+
+// TODO: Transform to JSON and return posts by topic
+func InfobaePostByTopic(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Infobae Post By Topic",
+	})
+}
