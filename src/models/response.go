d12ab08efe9fe9106bd13a5c1c8b6962261e25fd
jd-apprentice <contacto@jonathan.com.ar>
2024-11-23 05:13:50 -0300

    chore: models and typing

--- src/models/response.go
+++ src/models/response.go
@@ -2,29 +2,28 @@ package models
 
 import (
 	"encoding/xml"
-
-	"gorm.io/gorm"
 )
-
-type New struct {
-	gorm.Model
-	URL      string
-	Category string
-}
-
-type NewDetail struct {
-	gorm.Model
-	URL        string
-	LastMod    string
-	ChangeFreq string
-	Title      string
-}
 
 type SitemapIndex struct {
 	XMLName  xml.Name  `xml:"sitemapindex"`
 	Sitemaps []Sitemap `xml:"sitemap"`
 }
 
-type Sitemap struct {
+type NewsIndex struct {
+	XMLName xml.Name `xml:"urlset"`
+	URL     []URL    `xml:"url"`
+}
+
+type Location struct {
 	Loc string `xml:"loc"`
+}
+
+type URL struct {
+	Location
+	LastMod    string `xml:"lastmod"`
+	ChangeFreq string `xml:"changefreq"`
+}
+
+type Sitemap struct {
+	Location
 }
