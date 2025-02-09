016a099d693fb0814a2b59db26716fae17d8271d
jd-apprentice <contacto@jonathan.com.ar>
2025-02-09 02:21:37 -0300

    chore: precommit

--- src/constants/messages.go
+++ src/constants/messages.go
@@ -1,6 +1,6 @@
 package constants
 
 const (
-	WelcomeMessage = "Welcome to Infobae API"
+	WelcomeMessage  = "Welcome to Infobae API"
 	LastPostMessage = "This is the last Infobae post from /xml/news"
 )
